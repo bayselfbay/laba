d7ff731ad3901642d65feb3562c31beb21b5d9fe
bayselfbay <bayanaivanova465@gmail.com>
2024-04-19 11:15:32 +0300

    терминал

--- bayselfbay.cpp
+++ bayselfbay.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "rus");
 	float widht, hight, area, perimetr, diagonal;
-	cout << "Введите ширину прямоугольника" << endl;
+	wcout << L"Введите ширину прямоугольника" << endl;
 	cin >> widht;
-	cout << "Введите длину прямоугольника" << endl;
+	wcout << L"Введите длину прямоугольника" << endl;
 	cin >> hight;
 	//Тест на дурака
 	if (widht <= 0 || hight <= 0) {
@@ -16,8 +16,8 @@ int main() {
 	area = widht * hight; //Нахождение площади
 	perimetr = 2 * (widht + hight);//Нахождение периметра
 	diagonal = sqrt(widht * widht + hight * hight);//Нахождение длины диагонали
-	cout << "Площадь прямоугольника" << " " << area << endl;
-	cout << "Периметр прямоугольника" << " " << perimetr << endl;
-	cout << "Длина диагонали" << " " << diagonal << endl;
+	wcout << L"Площадь прямоугольника" << " " << area << endl;
+	wcout << L"Периметр прямоугольника" << " " << perimetr << endl;
+	wcout << L"Длина диагонали" << " " << diagonal << endl;
 	return 0;
 }
